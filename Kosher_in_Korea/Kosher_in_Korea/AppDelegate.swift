27a5ef1dba590668ca77120f0658f8f2115c6d2b
김우용 <kwyong@gim-uyong-ui-MacBookAir.local>
2023-07-22 12:10:18 +0900

    feat:페이스북 구글 로그인 추가

--- Kosher_in_Korea/Kosher_in_Korea/AppDelegate.swift
+++ Kosher_in_Korea/Kosher_in_Korea/AppDelegate.swift
@@ -6,19 +6,66 @@
 //
 
 import UIKit
+import GoogleSignIn
+import FacebookCore
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
+    func application(
+      _ app: UIApplication,
+      open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+    ) -> Bool {
+        //페이스북
+        ApplicationDelegate.shared.application(
+                    app,
+                    open: url,
+                    sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+                    annotation: options[UIApplication.OpenURLOptionsKey.annotation]
+                )
+        //구글
+      var handled: Bool
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+      handled = GIDSignIn.sharedInstance.handle(url)
+      if handled {
         return true
+      }
+
+      // Handle other custom URL types.
+
+      // If not handled by this app, return false.
+      return false
+    
+    }
+    
+    func application(
+      _ application: UIApplication,
+      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        //페이스북
+        ApplicationDelegate.shared.application(
+                    application,
+                    didFinishLaunchingWithOptions: launchOptions
+                )
+
+        //구글
+      GIDSignIn.sharedInstance.restorePreviousSignIn { user, error in
+        if error != nil || user == nil {
+          // Show the app's signed-out state.
+        } else {
+          // Show the app's signed-in state.
+        }
+      }
+        
+      return true
     }
 
     // MARK: UISceneSession Lifecycle
 
+    
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
